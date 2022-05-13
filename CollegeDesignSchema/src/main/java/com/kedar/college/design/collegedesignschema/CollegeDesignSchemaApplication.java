483a37dd24889d862675c184ebcccfc6e85e8a1e
kedarerande <erande.kedar@gmail.com>
2022-05-13 06:52:11 +0530

    Version 1- college db schema

---
+++ CollegeDesignSchema/src/main/java/com/kedar/college/design/collegedesignschema/CollegeDesignSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.kedar.college.design.collegedesignschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeDesignSchemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeDesignSchemaApplication.class, args);
+    }
+
+}
